ef9e71a3765444abea21f27dfce9c2a848529db6
fokayx <fokayx@gmail.com>
2015-08-29 20:25:57 +0800

    Initial Commit

---
+++ TABELogK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TABELogK
+//
+//  Created by oKay on 8/29/15.
+//  Copyright (c) 2015 fokayx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TABELogK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TABELogK
+//
+//  Created by oKay on 8/29/15.
+//  Copyright (c) 2015 fokayx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
