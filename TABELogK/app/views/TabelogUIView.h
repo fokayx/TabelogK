2931457a5d7fa555b912d672b0bc8b81f942c1aa
fokayx <fokayx@gmail.com>
2015-09-04 18:12:19 +0800

    rails style file manager

--- TABELogK/app/views/TabelogUIView.h
+++ TABELogK/app/views/TabelogUIView.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface TabelogUIView : UIView
+
 +(UITextField *)signInTextField:(UITextField *)textField;
 +(UIButton *)mainSocialButton:(UIButton *)socialButton image:(UIImage *)image;
 +(UIButton *)customUIButton:(UIButton *)button borderWidth:(int)width borderColor:(UIColor *)color boderradius:(int)radius;
+
 @end
